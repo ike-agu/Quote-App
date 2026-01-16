e1433e4b1ec00f4cb12aed118b9be5d99b65762a
ike-agu <ikennaww@yahoo.fr>
2026-01-16 03:12:45 +0000

    Add frontend form to POST new quotes to the backend

--- frontend/quoteApi.js
+++ frontend/quoteApi.js
@@ -1,13 +1,20 @@
 const quoteElement = document.getElementById("quote");
-const authorElement = document.getElementById("author")
-const newQuoteBtn = document.getElementById("new-quote")
+const authorElement = document.getElementById("author");
+const newQuoteBtn = document.getElementById("new-quote");
+
+const quoteInput = document.getElementById("quote-input");
+const authorInput = document.getElementById("author-input");
+const addQuoteBtn = document.getElementById("submit-btn");
+const messageElement = document.getElementById("message");
+
+const form = document.getElementById("add-quote-form");
 
 const QUOTE_API_URL = "http://127.0.0.1:3000";
 
-async function displayRandomQuote(){
+async function displayRandomQuote() {
   try {
     const res = await fetch(`${QUOTE_API_URL}/quote`);
-    const data = await res.json()
+    const data = await res.json();
 
     quoteElement.textContent = `"${data.quote}"`;
     authorElement.textContent = `-${data.author}`;
@@ -19,6 +26,44 @@ async function displayRandomQuote(){
 }
 
 displayRandomQuote();
-
 // Load a new quote when button is clicked
 newQuoteBtn.addEventListener("click", displayRandomQuote);
+
+//===Add quote=========
+async function addQuote(event) {
+  // if (event) event.preventDefault();
+  const quote = quoteInput.value.trim();
+  const author = authorInput.value.trim();
+
+  if (!quote || !author) {
+    messageElement.textContent = "Please enter both quote and an author";
+    return;
+  }
+
+  try {
+    const res = await fetch(`${QUOTE_API_URL}/quote`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ quote, author }),
+    });
+
+    const data = await res.json();
+    if (!res.ok) {
+      messageElement.textContent = data.error || "Failed to add quote";
+      return;
+    }
+    messageElement.textContent = "Quote added successfully!";
+    //clear inputs
+    quoteInput.value = "";
+    authorInput.value = "";
+
+    displayRandomQuote();
+  } catch (error) {
+    messageElement.textContent = "Error adding quote";
+    console.error(error);
+  }
+}
+//add new quote when button is clicked
+addQuoteBtn.addEventListener("click", addQuote);
+
+form.addEventListener("submit", addQuote);
